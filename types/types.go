b740b9c7589b435ec8f728ac320d93c067d6ecb7
Kai <b02901184@ntu.edu.tw>
2023-12-20 23:34:06 +0800

    Add types of database tables

--- types/types.go
+++ types/types.go
@@ -6,3 +6,23 @@ type Block struct {
 	BlockTime  int64  `gorm:"not null"`
 	ParentHash string `gorm:"not null"`
 }
+
+type Log struct {
+	Data  string `bson:"data"`
+	Index int    `bson:"index"`
+}
+
+type Transaction struct {
+	Hash  string `bson:"tx_hash"`
+	From  string `bson:"from"`
+	To    string `bson:"to"`
+	Value string `bson:"value"`
+	Nonce int    `bson:"nonce"`
+	Data  string `bson:"data"`
+	Logs  []Log  `bson:"logs"`
+}
+
+type BlockTransactions struct {
+	BlockNum     int      `bson:"block_num"`
+	Transactions []string `bson:"transactions"`
+}
